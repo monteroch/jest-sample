019617b55036ecb386b587c19bc4670a7db7801a
Christian Montero Martínez <christianmontero08@gmail.com>
2020-07-27 20:57:24 -0700

    Add more testing mocking functions

--- script2.test.js
+++ script2.test.js
@@ -16,3 +16,17 @@ it('calls swapi to get people with a promise', () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+it('getPeople returns count and results', () => {
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        count: 87,
+        results: [0, 1, 2, 3, 4, 5]
+      })
+    }))
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+      expect(mockFetch.mock.calls.length).toBe(1);
+      expect(mockFetch).toBeCalledWith('http://swapi.py4e.com//api/people')
+    });
+});
